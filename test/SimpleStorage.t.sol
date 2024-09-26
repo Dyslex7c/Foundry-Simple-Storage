776674a540bb644b8de6115e61750982664efad4
Dyslex7c <polynsominiac@gmail.com>
2024-09-26 17:16:32 +0530

    finalized

--- test/SimpleStorage.t.sol
+++ test/SimpleStorage.t.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
-import { Test } from "forge-std/Test.sol";
+import {Test} from "forge-std/Test.sol";
 import {SimpleStorage} from "../src/SimpleStorage.sol";
 
 contract SimpleStorageTest is Test {
